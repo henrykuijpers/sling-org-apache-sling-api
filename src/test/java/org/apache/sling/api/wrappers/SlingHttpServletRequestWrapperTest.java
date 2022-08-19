f8bdb9c82fb4eafbdefb23aa8c43401648735a12
Henry Kuijpers <Henry.Kuijpers@amplexor.com>
2022-08-19 16:38:21 +0200

    SLING-9036 Failing test case for SlingHttpServletRequestWrapper - It demonstrates that the non-wrapped request is used to perform the adaption

---
+++ src/test/java/org/apache/sling/api/wrappers/SlingHttpServletRequestWrapperTest.java
@@ -0,0 +1,57 @@
+package org.apache.sling.api.wrappers;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.adapter.AdapterManager;
+import org.apache.sling.api.adapter.SlingAdaptable;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SlingHttpServletRequestWrapperTest {
+    @Mock
+    private AdapterManager adapterManager;
+
+    @Mock
+    private SlingHttpServletRequest request;
+
+    @Test
+    public void adaptToShouldUseWrapperAsSourceForAdaption() {
+        final SlingHttpServletRequest underTest = new SlingHttpServletRequestWrapper(request) {
+            @Override
+            public String getRequestURI() {
+                return super.getRequestURI() + "/1234";
+            }
+        };
+
+        when(adapterManager.getAdapter(any(SlingHttpServletRequest.class), eq(MyModel.class)))
+            .thenAnswer(invocationOnMock ->
+                new MyModel(((SlingHttpServletRequest)invocationOnMock.getArgument(0)).getRequestURI()));
+        SlingAdaptable.setAdapterManager(adapterManager);
+        when(request.adaptTo(MyModel.class)).thenReturn(new MyModel("Failure - The wrapper should be calling the AdapterManager"));
+        when(request.getRequestURI()).thenReturn("/test");
+
+        final MyModel result = underTest.adaptTo(MyModel.class);
+        assertNotNull("A model instance should be returned", result);
+        assertEquals("The model should have considered the overwritten path","/test/1234", result.getPath());
+    }
+
+    private static class MyModel {
+        private final String path;
+
+        public MyModel(final String path) {
+            this.path = path;
+        }
+
+        public String getPath() {
+            return this.path;
+        }
+    }
+}
